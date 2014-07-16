09a5c523fa611d2dca2c2732434937fa19fc7532
Ammar Javed <ammarj965@gmail.com>
2014-07-16 16:21:21 -0400

    new slider + tweaks
    Added bxslider for the content cycling, worked on the logo and the top
    bar a bit more.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
-
-$(function() {
-// Slow slides for internal links
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top - 30
-            }, 1000);
-            if ($(window).width() < 600)
-            {
-                $('nav ul').slideUp();
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+  $(function() {
+        // Slow slides for internal links
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 30
+                    }, 1000);
+                    if ($(window).width() < 600)
+                    {
+                        $('nav ul').slideUp();
+                    }
+                    return false;
+                }
             }
-            return false;
-        }
-    }
-}); 
+        }); 
+    });
 });
+$(document).foundation();

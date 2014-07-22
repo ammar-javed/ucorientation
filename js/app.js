f98979d80b2139148c553590e663b1688a8d42d9
Ammar Javed <ammarj965@gmail.com>
2014-07-22 00:53:49 -0400

    Registration nearly complete
    Token being created successfully; form needs to be properly filled out.
    
    left to do; charging + sending data to doc and all the transitions on
    the front end.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).ready(function(){
-  var slider = $('.bxslider').bxSlider({
+  var aboutslider = $('.bxslider').bxSlider({
                                         mode: "fade",
                                         adaptiveHeight: true,
                                         controls: false,
                                         pager: true
                                         });
+
+
+  var registrationSlider = $('.registration-slider').bxSlider({
+                                      mode: "fade",
+                                      adaptiveHeight: true,
+                                      controls: false,
+                                      pager: false
+                                      });
+
   $(function() {
         // Slow slides for internal links
         $('a[href*=#]:not([href=#])').click(function() {
@@ -27,14 +36,69 @@ $(document).ready(function(){
         }); 
     });
   $("#history-link").click(function() {
-    slider.goToSlide(0);
+    aboutslider.goToSlide(0);
   });
   $("#residence-link").click(function() {
-    slider.goToSlide(1);
+    aboutslider.goToSlide(1);
   });
   $("#commuter-link").click(function() {
-    slider.goToSlide(2);
+    aboutslider.goToSlide(2);
   });
+
+  $('#form-one').on('invalid', function(){
+    registrationSlider.redrawSlider();
+  });
+
+  $('#form-one').on('vaild', function(){
+    registrationSlider.redrawSlider();
+  });
+
+  $('#payment-form').on('valid', function(){
+    registrationSlider.redrawSlider();
+  });
+
+  var $input = $('#payment-form :input:text'),
+      $register = $('#finalizeReg');
+
+  $register.attr('disabled', true);
+  $input.keyup(function() {
+      var trigger = false;
+      $input.each(function() {
+          if (!$(this).val()) {
+              trigger = true;
+          }
+      });
+      trigger ? $register.attr('disabled', true) : $register.removeAttr('disabled');
+  });
+
+  $('#registerNext').click(function(){
+    // //Let script know a form was sent
+    // $("#form-one").append("<input type='hidden' name='form' value='true' />");
+    // //serialize and send
+    // formData = $('#form-one').serialize();
+    // $.ajax({
+    //   url : 'scripts/register_validation.php',
+    //   type: 'POST',
+    //   data: formData,
+    //   success: function(data, textStatus, jqXHR){
+    //     reply = JSON.parse(data);
+    //     console.log(data);
+    //     console.log(reply);
+    //     if (reply.result == "success")
+    //     {
+          registrationSlider.goToSlide(1);
+    //     } else {
+    //       $.each(reply, function(i, val) {
+    //         $("." + i).addClass(" " + val);
+    //       });
+    //     }
+    //   },
+    //   error: function(jqXHR, textStatus, errorThrown) {
+    //     registrationSlider.goToSlide(3);
+    //   }
+    // });
+  });
+
 });
 
 $(document).foundation();
---
+++ scripts/register_validation.php
@@ -0,0 +1,65 @@
+<?php
+
+$result = 'success';
+$returnArray = array();
+$phonePattern = '/^\+?\d+(-\d+)*$/';
+$mmddyyyyPattern = '/^(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.]\d{4}$/';
+$namePattern = '/[a-zA-Z]+/';
+
+if($_POST['form'])
+{
+	if(!preg_match($phonePattern, $_POST['homePhone'])){
+		$returnArray['homePhone'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($phonePattern, $_POST['cellPhone'])){
+		$returnArray['cellPhone'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($phonePattern, $_POST['emergencyPhone'])){
+		$returnArray['emergencyPhone'] = 'error';
+		$result = 'error';
+	}
+	if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+		$returnArray['email'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($namePattern, $_POST['emergencyFirstName'])){
+		$returnArray['emergencyFirstName'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($namePattern, $_POST['firstName'])){
+		$returnArray['firstName'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($namePattern, $_POST['lastName'])){
+		$returnArray['lastName'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($namePattern, $_POST['emergencyLastName'])){
+		$returnArray['emergencyLastName'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($namePattern, $_POST['emergencyRelation'])){
+		$returnArray['emergencyRelation'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($namePattern, $_POST['gender'])){
+		$returnArray['gender'] = 'error';
+		$result = 'error';
+	}
+	if(!preg_match($mmddyyyyPattern, $_POST['birthDate'])){
+		$returnArray['birthDate'] = 'error';
+		$result = 'error';
+	}
+
+	$returnArray['result'] = $result;
+}
+else
+{
+	$result = 'error';
+	$returnArray['result'] = $result;
+}
+echo json_encode($returnArray);
+
+?>

47fab88d76ec1b305fe52b401c805b361ac53f89
Ammar Javed <ammarj965@gmail.com>
2014-07-24 23:11:56 -0400

    Social media links added in

--- js/app.js
+++ js/app.js
@@ -176,6 +176,17 @@ $(document).ready(function(){
     return false;
   });
 
+  var s = $("#sticker");
+  var pos = s.position();                    
+  $(window).scroll(function() {
+      var windowpos = $(window).scrollTop();
+      if (windowpos >= pos.top) {
+          s.addClass("stick");
+      } else {
+          s.removeClass("stick"); 
+      }
+  });
+
 });
 
 $(document).foundation();
